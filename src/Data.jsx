2233753a13740019ad3943a2358741c75bac29ee
Dux <kartikmadaan1907@gmail.com>
2023-04-07 00:05:00 +0530

    Initial commit

---
+++ src/Data.jsx
@@ -0,0 +1,40 @@
+const Data = [
+  {
+    title: "Accordion",
+    description:
+      "The accordion uses collapse internally to make it collapsible.",
+    img: "https://www.tutorialrepublic.com/lib/images/bootstrap-5/bootstrap-accordion-menu.png",
+  },
+  {
+    title: "Button",
+    description:
+      "Bootstrap has a base .btn class that sets up basic styles such as padding and content alignment.",
+    img: "https://i.stack.imgur.com/k0Udi.png",
+  },
+  {
+    title: "Navbar",
+    description:
+      "Navbars require a wrapping .navbar with .navbar-expand{-sm|-md|-lg|-xl|-xxl} for responsive collapsing ",
+    img: "https://www.devwares.com/blog/images/bootstrap-navbar-with-contrast.png",
+  },
+  {
+    title: "Progress bar",
+    description:
+      "We use the .progress as a wrapper to indicate the max value of the progress bar.",
+    img: "https://media.geeksforgeeks.org/wp-content/uploads/20190502150706/progress4.png",
+  },
+  {
+    title: "Borders",
+    description:
+      "Use border utilities to add or remove an element’s borders. Choose from all borders or one at a time.",
+    img: "https://media.geeksforgeeks.org/wp-content/uploads/Screenshot-14-15-e1547624651876.png",
+  },
+  {
+    title: "Position",
+    description:
+      "Arrange elements easily with the edge positioning utilities. The format is {property}-{position}.",
+    img: "https://wonderdevelop.com/wp-content/uploads/2022/04/CSS-Position-Property.jpg",
+  },
+];
+
+export default Data;
