946881b16946f1eb2bd7933c96130d3cf3a2cac5
Dux <kartikmadaan1907@gmail.com>
2023-04-08 02:15:01 +0530

    Added default path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Service from "./Service";
